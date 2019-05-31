c25f69482d5e14cc02e953e682e767e57b737916
Josef Taylor <josef.taylor@burohappold.com>
2019-05-31 11:09:14 -0400

    Added Create BarResult Method

---
+++ S_Frame_Engine/Create/BarResult.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Structure.Results;
+using BH.oM.Geometry;
+
+namespace BH.Engine.S_Frame.Create
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+
+        public static BarResult BarResultFromForces(string name = null, Vector f = null, Vector m = null )
+        {
+            BarForce result = new BarForce()
+            {
+                ObjectId = name,
+                MX = m.X,
+                MY = m.Y,
+                MZ = m.Z,
+                FX = f.X,
+                FY = f.Y,
+                FZ = f.Z,
+            };
+
+            return result;
+
+        }
+
+    }
+}
